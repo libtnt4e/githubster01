a43eb25bab975b56e5af8af0ec56c5ef9dd2a3d1
libtnt4e@gmail.com <libtnt4e@gmail.com>
2025-06-05 15:01:17 -0700

    remove items

--- solution01.ps1
+++
@@ -1,9 +0,0 @@
-
-
-$pathToOutputFile = 'C:\Users\temp18\Downloads\out3.csv'
-$webResponse = Invoke-WebRequest -Uri "https://datausa.io/api/data?drilldowns=State&measures=Population"
-$webResponse.Content | Out-File C:\Users\temp18\Documents\Github\githubster01\parseDemo.json
-$obj03 = Get-Content C:\Users\temp18\Downloads\parseDemo.json -Raw | ConvertFrom-Json  
-$obj03 | Select-Object -Property * |  Select-Object -ExpandProperty data|  Export-Csv -Path 'C:\Users\temp18\Downloads\out3.csv' -NoTypeInformation
-
-
